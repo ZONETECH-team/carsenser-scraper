c251dfa8c238aadba39a68e7112a360fc7b0c04d
zonehisa <nijipapazoneokun123@gmail.com>
2026-07-06 10:44:30 +0900

    Fix cron scraper path

--- run_scraper.sh
+++ run_scraper.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 export PATH=/usr/local/bin:/usr/bin:/bin:/opt/homebrew/bin
-cd /Users/iroiropro/dev/carsenser-scraper
+cd /Users/iroiropro/ghq/github.com/ZONETECH-team/clients/autobody-sugawara/carsenser-scraper
 echo "$(date) cron reached script" >> /tmp/cron_debug.log
 # pyenvはcron環境では動かないためvenvのPythonを直接使う
-source /Users/iroiropro/dev/carsenser-scraper/.venv/bin/activate
-/Users/iroiropro/dev/carsenser-scraper/.venv/bin/python /Users/iroiropro/dev/carsenser-scraper/scraper.py >> /tmp/carsenser_test.log 2>&1
+source /Users/iroiropro/ghq/github.com/ZONETECH-team/clients/autobody-sugawara/carsenser-scraper/.venv/bin/activate
+/Users/iroiropro/ghq/github.com/ZONETECH-team/clients/autobody-sugawara/carsenser-scraper/.venv/bin/python /Users/iroiropro/ghq/github.com/ZONETECH-team/clients/autobody-sugawara/carsenser-scraper/scraper.py >> /tmp/carsenser_test.log 2>&1
